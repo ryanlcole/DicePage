5788f1c0097fcb76ee12de31d38f547735034910
ryanlcole <ryanlcole@gmail.com>
2026-08-13 09:34:28 -0400

    Register HTTP client for RIST data

--- apps/rist-world/Program.cs
+++ apps/rist-world/Program.cs
@@ -5,4 +5,5 @@ using RistWorld;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 await builder.Build().RunAsync();
