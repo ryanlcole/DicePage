d9d513ec8bc34c1ce98e5f7fe38609aa9e3afb9f
ryanlcole <ryanlcole@gmail.com>
2026-08-13 06:35:29 -0400

    Register Naeja Drive map for WORLD prototype

---
+++ apps/tactical/js/naeja_world_asset.js
@@ -0,0 +1,8 @@
+export const NAEJA_WORLD_MAP = "https://drive.google.com/uc?export=view&id=1tESehBzxAPrugiE4gNL9Vsfc5Mmh0tCT";
+export const NAEJA_WORLD_META = Object.freeze({
+  sourceAuthority: "google_drive",
+  driveFileId: "1tESehBzxAPrugiE4gNL9Vsfc5Mmh0tCT",
+  sourceName: "Naeja.PNG",
+  width: 961,
+  height: 541
+});
