c0ab49a09322e5e8ec5838d2bcb03a27a38322be
ryanlcole <ryanlcole@gmail.com>
2026-08-22 20:50:17 -0400

    Register Discord authentication client

--- apps/rist-world/Program.cs
+++ apps/rist-world/Program.cs
@@ -6,5 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<DiscordAuthClient>();
 builder.Services.AddScoped<WorldSession>();
 await builder.Build().RunAsync();
