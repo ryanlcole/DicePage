5a41f23848d8f083d29917144b72d1e684010529
ryanlcole <ryanlcole@gmail.com>
2026-09-15 18:49:44 -0400

    Cache-bust World Builder meaning keyboards

--- apps/rist-world/wwwroot/worldbuilder-controls.js
+++ apps/rist-world/wwwroot/worldbuilder-controls.js
@@ -1,5 +1,5 @@
 import './worldbuilder-projection.js?v=20260912-parallax-shared-depth-1';
-import './worldbuilder-context-keyboard.js?v=20260915-viewer-root-1';
+import './worldbuilder-context-keyboard.js?v=20260915-meaning-keyboards-2';
 import './rist-card-tiff.js';
 
 window.ristWorld=window.ristWorld||{};
